6caeaf331cdc365f456da003197ead06a6a8fa12
vkd225 <vkd225@nyu.edu>
2017-03-28 14:14:54 -0400

    update search and add features

--- Collection/mainSearch.py
+++ Collection/mainSearch.py
@@ -4,11 +4,15 @@ s3 = boto3.resource('s3')
 SOURCEBUCKET = "searchimagestricon"
 COLLECTION = "TriconHeroesCollection"
 
+# ------------------- Upload Source Image to the Source Bucket --------------------------------------
+
 KEY_SOURCE = raw_input("Enter Imagename: ")
 s3.meta.client.upload_file(KEY_SOURCE, 'searchimagestricon', KEY_SOURCE)
 print ("File Uploaded")
 
 
+
+# ----------------------- Search faces in the Collection ---------------------------------------------
 def search_faces_by_image(bucket, key, collection_id, threshold=80, region="us-west-2"):
 	rekognition = boto3.client("rekognition", region)
 	response = rekognition.search_faces_by_image(
@@ -23,12 +27,39 @@ def search_faces_by_image(bucket, key, collection_id, threshold=80, region="us-w
 	)
 	return response['FaceMatches']
 
-for record in search_faces_by_image(SOURCEBUCKET, KEY_SOURCE, COLLECTION):
-	print len(record)
-	face = record['Face']
-	print len(face)
-	print "Matches: {}".format(face['ExternalImageId']) + " with Similiraty: ({}%)".format(record['Similarity'])
-	#print "  FaceId : {}".format(face['FaceId'])
-	#print "  ImageId : {}".format(face['ExternalImageId'])
+
+# ---------------------------- Check the input if it is a face or not ----------------------------------
+# Detect labels of the input image
+
+def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="us-west-2"):
+	rekognition = boto3.client("rekognition", region)
+	response = rekognition.detect_labels(
+		Image={
+			"S3Object": {
+				"Bucket": bucket,
+				"Name": key,
+			}
+		},
+		MaxLabels=max_labels,
+		MinConfidence=min_confidence,
+	)
+	return response['Labels']
 
 
+
+# --------------------- Similarity with collection images. Confidence of the source image -------------------
+
+for label in detect_labels(SOURCEBUCKET, KEY_SOURCE):
+	# print "{Name} - {Confidence}%".format(**label)
+	if "Person" in label.values():
+		print "{Name} - {Confidence}%".format(**label)
+
+		for record in search_faces_by_image(SOURCEBUCKET, KEY_SOURCE, COLLECTION):
+			face = record['Face']
+			print "Matches: {}".format(face['ExternalImageId']) + " with Similiraty: ({}%)".format(record['Similarity'])
+			#print "  FaceId : {}".format(face['FaceId'])
+			#print "  ImageId : {}".format(face['ExternalImageId'])
+
+
+	else:
+		print "{Name} - {Confidence}%".format(**label)
